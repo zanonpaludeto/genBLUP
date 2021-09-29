05b194b220bc1f52a2d6402fe38cb539abfe649c
zanonpaludeto <90414426+zanonpaludeto@users.noreply.github.com>
2021-09-29 11:01:48 -0300

    exclude "tibble" from p_load pakckages

--- genBLUP.R
+++ genBLUP.R
@@ -5,7 +5,7 @@ genBLUP <- function(data, varResp, treatment = c("Prog","Clone"), plotType = c("
   
   # loading packages --------------------------------------------------------
   
-  pacman::p_load(future,sommer,stringr,tibble,tidyverse)
+  pacman::p_load(future,sommer,stringr,tidyverse)
   
   # stops and warnings ------------------------------------------------------
   
